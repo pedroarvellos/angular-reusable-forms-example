020cf10360335c6392aa40993649cbce6e6cce09
pgsantosarvellos <pedrogoncalves.santosarvellos@altran.com>
2020-03-23 12:04:53 +0000

    Use metadata

--- reusable-forms/src/app/custom-input/custom-input.component.ts
+++ reusable-forms/src/app/custom-input/custom-input.component.ts
@@ -1,15 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
 @Component({
-  selector: 'custom-input',
-  templateUrl: './custom-input.component.html',
-  styleUrls: ['./custom-input.component.css']
+  selector: "custom-input",
+  templateUrl: "./custom-input.component.html",
+  styleUrls: ["./custom-input.component.css"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: CustomInputComponent
+    }
+  ]
 })
-export class CustomInputComponent {
-  @Input() type = 'text';
+export class CustomInputComponent implements ControlValueAccessor {
+  disabled;
+  @ViewChild("input", { static: false }) input: ElementRef;
+  @Input() type = "text";
   @Input() isRequired: boolean = false;
   @Input() pattern: string = null;
   @Input() label: string = null;
   @Input() placeholder: string;
   @Input() errorMsg: string;
+
+  writeValue(obj: any): void {
+    this.input.nativeElement.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onChange(event) {}
+  onTouched() {}
 }
